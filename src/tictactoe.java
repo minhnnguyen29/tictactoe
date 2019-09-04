9ea74e98a90b4089e9cb49e7f3d279c6908df4ea
minhn <minhnnguyen29@gmail.com>
2019-09-04 11:05:09 +1000

    This contains env to to create a tictactoe game: - IntelliJ congifuration files. - class tictactoe

---
+++ src/tictactoe.java
@@ -0,0 +1,10 @@
+class tictactoe {
+    char[][] game;
+    public static void main(String[] args) {
+        tictactoe ttt = new tictactoe(); //create an instace of class
+
+    }
+    public void drawBoard(){
+        
+    }
+}//end of class tictactoe
